673ca2c442ce07b4d4d4875d6960fc286eb37e9f
LCHRT <56543198+Lcc-code@users.noreply.github.com>
2021-09-02 15:42:20 +0800

    main

---
+++ 个数统计/maximumProduct.cc
@@ -0,0 +1,17 @@
+/*
+实现 int sqrt(int x) 函数。
+
+计算并返回 x 的平方根，其中 x 是非负整数。
+
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+*/
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end(), [](int a, int b) -> bool {return a < b;});
+
+        int n = nums.size();
+        return std::max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+};
--- 个数统计/mySqrt.cc
+++ 个数统计/mySqrt.cc
@@ -9,7 +9,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
-
+        int index = -1;
+        int l = 0, r = x;
+        while (l <= r)
+        {
+            double mid = l + (r - l)/2;
+            if (mid * mid <= x)
+            {
+                index = mid;
+                l = mid + 1;
+            } else
+            {
+                r = mid - 1;
+            }
+        }
+        return index;        
     }
 };
